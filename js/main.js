2cacea37367648c61effed0c60a734b4e1d952f7
Jonathan Meine <jrmeine@gmail.com>
2017-12-27 14:26:40 -0700

    Basic project

---
+++ js/main.js
@@ -0,0 +1,15 @@
+lightbox.option({
+      'resizeDuration': 200,
+      'wrapAround': true,
+      'maxWidth': 950,
+      'positionFromTop': 125,
+      'fadeDuration': 1000,
+      'imageFadeDuration': 1000,
+      'alwaysShowNavOnTouchDevices': true,
+      'albumLabel': '',
+      'fitImagesInViewport': true
+      });
+
+$(document).ready(function() {
+	$('#search').hideseek();
+});
